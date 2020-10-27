7a6fc5ef9015092097b89a9d6b17b5ca00f1e241
hexi03 <62556634+hexi03@users.noreply.github.com>
2020-10-27 20:45:46 +0300

    Update tempmail.py

--- tempmail.py
+++ tempmail.py
@@ -16,7 +16,7 @@ class TempMail(object):
     Default value is ``api2.temp-mail.org``.
     """
 
-    def __init__(self, login=None, domain=None, api_domain='api2.temp-mail.org'):
+    def __init__(self, login=None, domain=None, api_domain='privatix-temp-mail-v1.p.rapidapi.com'):
         self.login = login
         self.domain = domain
         self.api_domain = api_domain
@@ -30,13 +30,17 @@ class TempMail(object):
         Return list of available domains for use in email address.
         """
         if not hasattr(self, '_available_domains'):
-            url = 'http://{0}/request/domains/format/json/'.format(
+            url = 'https://{0}/request/domains/'.format(
                 self.api_domain)
-            req = requests.get(url)
+            req = requests.request("GET",url,headers=self.headers)
             domains = req.json()
             setattr(self, '_available_domains', domains)
         return self._available_domains
-
+    def set_header(self,host,key):
+        self.headers = {
+            'x-rapidapi-host': host,
+            'x-rapidapi-key': key
+        }
     def generate_login(self, min_length=6, max_length=10, digits=True):
         """
         Generate string for email address login with defined length and
@@ -76,7 +80,7 @@ class TempMail(object):
 
         :param email: email address for generate md5 hash.
         """
-        return md5(email).hexdigest()
+        return md5(email.encode('utf-8')).hexdigest()
 
     def get_mailbox(self, email=None, email_hash=None):
         """
@@ -91,7 +95,8 @@ class TempMail(object):
         if email_hash is None:
             email_hash = self.get_hash(email)
 
-        url = 'http://{0}/request/mail/id/{1}/format/json/'.format(
+        url = 'https://{0}/request/mail/id/{1}/'.format(
             self.api_domain, email_hash)
-        req = requests.get(url)
+        req = requests.get(url,headers=self.headers)
         return req.json()
+
